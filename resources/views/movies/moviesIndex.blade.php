1dda9923fcad9d6fb69894cab62a511b69519ed2
JorgePaladines <jpaladin@espol.edu.ec>
2019-01-09 11:35:28 -0500

    Se puede ver la lista de peliculas y la pelicula individual. Sacar el top 10

--- resources/views/movies/moviesIndex.blade.php
+++ resources/views/movies/moviesIndex.blade.php
@@ -1,3 +1,15 @@
-<h1>
-	List of Movies
-</h1>
+<h1>Movies</h1>
+    @if(count($movies) > 0)
+        @foreach($movies as $movie)
+            <div class="well">
+                <div class="row">
+                    <div class="col-md-8 col-sm-8">
+                        <h3><a href="/movies/{{$movie->id}}">{{$movie->titulo}}</a></h3>
+                        <small>Written on {{$movie->created_at}}</small>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    @else
+        <p>No movies found</p>
+@endif
